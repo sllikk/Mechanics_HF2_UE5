dd7aefc5d755a1b60c16d52bcd73016c60373e60
slickk <fddftrrfg@gmail.com>
2024-03-23 23:48:48 +0200

    Door

--- Source/JobsGame/Private/WorldActors/Door.cpp
+++ Source/JobsGame/Private/WorldActors/Door.cpp
@@ -1,12 +1,50 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "WorldActors/Door.h"
+
+DEFINE_LOG_CATEGORY(LOG_LOADING_RESOURCE);
+DEFINE_LOG_CATEGORY(LogDoor);
 
 ADoor::ADoor()
 {
- 	
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Loading resource and set StaticMesh
+	DoorComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorComponent"));
+	DoorFrameComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorFrameComponent"));
+	
+	TArray<FInfoLoadResourse> ResourseToLoad = {
+
+		{"/Game/WorldActors/Door/sDoor", nullptr},
+		{"/Game/WorldActors/Door/sDoorFrame", nullptr},
+		{"/Game/WorldActors/Door/DoorCurveFloat", nullptr},
+	};	
+	
+	for (FInfoLoadResourse& Resourse : ResourseToLoad)
+	{
+		Resourse.LoadingResource = LoadObject<UObject>(nullptr, *Resourse.ResourceLoadPath);
+		if (!Resourse.LoadingResource)
+		{
+			UE_LOG(LOG_LOADING_RESOURCE, Warning, TEXT("Eror find object !!!!!!!!!"))
+		}
+	}	
+	for (const FInfoLoadResourse Resourse : ResourseToLoad)
+	{
+		UStaticMesh* DoorMesh = Cast<UStaticMesh>(ResourseToLoad[0].LoadingResource);
+		UStaticMesh* DoorFrameMesh = Cast<UStaticMesh>(ResourseToLoad[1].LoadingResource);
+
+		if (DoorMesh)
+		{
+			DoorComponent->SetStaticMesh(DoorMesh);
+		}
+		if (DoorFrameMesh)
+		{
+			DoorFrameComponent->SetStaticMesh(DoorFrameMesh);
+		}
+	}
+	
+	SetRootComponent(DoorComponent);
+	DoorFrameComponent->SetupAttachment(DoorComponent);
 
 }
 
@@ -15,7 +53,37 @@ void ADoor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
+	TArray<FInfoLoadResourse> SoundResourceToLoad = {
+		
+		{"/Game/Sound/ActorSound/Cue/Door_move_Cue", nullptr},
+		{"/Game/Sound/ActorSound/Cue/Door_stop_Cue", nullptr},
+	};
+
+	for (FInfoLoadResourse& Resourse : SoundResourceToLoad)
+	{
+		Resourse.LoadingResource = LoadObject<UObject>(nullptr, *Resourse.ResourceLoadPath);
+		if (!Resourse.LoadingResource)
+		{
+			UE_LOG(LOG_LOADING_RESOURCE, Warning, TEXT("Eror find sound object !!!!!!!!!"));
+		}
+	}
+	for (const FInfoLoadResourse& Resourse : SoundResourceToLoad)
+	{
+		USoundBase* Door_Move = Cast<USoundBase>(SoundResourceToLoad[0].LoadingResource);
+		USoundBase* Door_Close = Cast<USoundBase>(SoundResourceToLoad[1].LoadingResource);
+
+		if (Door_Move)
+		{
+			OpenDoorSound = Door_Move;
+		}
+		if (Door_Close)
+		{
+			CloseDoorSound = Door_Close;
+		}
+
+	}
+
+}	
 
 
 void ADoor::Tick(float DeltaTime)
@@ -23,5 +91,17 @@ void ADoor::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	
+}
+
+void ADoor::Interact()
+{
+}
+
+void ADoor::OpenDoor(float Value)
+{
+}
+
+void ADoor::DoorOnsameSide()
+{
 }
 
--- Source/JobsGame/Public/WorldActors/Door.h
+++ Source/JobsGame/Public/WorldActors/Door.h
@@ -4,12 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Logging/LogMacros.h"
+#include "Components/TimelineComponent.h"
 #include "Door.generated.h"
+class UStaticMeshComponent;
+class UBoxComponent;
+class USoundBase;
+class UCurveFloat;
+struct FTimeline;
+
+DECLARE_LOG_CATEGORY_EXTERN(LOG_LOADING_RESOURCE, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogDoor, Log, All);
 
 UCLASS()
 class JOBSGAME_API ADoor : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditInstanceOnly, Category = "StaticMesh", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> DoorComponent;
+	
+	UPROPERTY(EditInstanceOnly, Category = "StaticMesh", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> DoorFrameComponent;
+
+	UPROPERTY(EditInstanceOnly, Category = "Collision", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> BoxCollision;
+
 	
 public:	
 	
@@ -22,5 +42,36 @@ protected:
 public:	
 
 	virtual void Tick(float DeltaTime) override;
+	
+	void Interact();
 
+public:	
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Audio")
+	TObjectPtr<USoundBase> OpenDoorSound;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Audio")
+	TObjectPtr<USoundBase> CloseDoorSound;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UCurveFloat> CurveFloat;
+	
+	FTimeline Timeline;
+	
+	void OpenDoor(float Value);
+	void DoorOnsameSide();
+
+	bool blsDoorClossed;
+	bool blsDoorOnsameSide;
+	float m_DoorRotateAngle = 90.0f;
+
+	
+};
+
+struct FInfoLoadResourse
+{
+	FString ResourceLoadPath; 
+	UObject* LoadingResource;
 };
