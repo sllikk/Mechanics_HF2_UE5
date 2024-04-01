879be2f61b29a97e51628bc4eacc3d8b68e6b266
Sllikk <fddftrrfg@gmail.com>
2024-04-01 23:48:59 +0300

    Physics Handle

---
+++ Source/JobsGame/Private/PlayerComponent/PhysicsHandle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "PhysicsHandle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPhysicsHandle : public UPhysicsHandleComponent
+{
+	GENERATED_BODY()
+	
+};
