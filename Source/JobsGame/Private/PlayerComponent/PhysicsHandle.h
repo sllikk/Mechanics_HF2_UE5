ca45a5b1bf272380e05f3ce1b3733e4ba2c80cc2
Sllikk <fddftrrfg@gmail.com>
2024-04-02 00:35:09 +0300

    Work a PhysicsHandle

--- Source/JobsGame/Private/PlayerComponent/PhysicsHandle.h
+++ Source/JobsGame/Private/PlayerComponent/PhysicsHandle.h
@@ -4,14 +4,54 @@
 
 #include "CoreMinimal.h"
 #include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Player/MyCharacter.h"
 #include "PhysicsHandle.generated.h"
+class AMyCharacter;
+class USoundBase;
+class UInputMappingContext;
+class UInputAction;
 
-/**
- * 
- */
-UCLASS()
+DECLARE_LOG_CATEGORY_EXTERN(LogPhysicsHandle, Log, All);
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class UPhysicsHandle : public UPhysicsHandleComponent
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "MappingContext", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> PhysicsHandleMappingContext;	
+	
+	UPROPERTY(EditAnywhere, Category = "InputAction", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> ToggleGrabAction; 
+
+	UPROPERTY(EditDefaultsOnly, Category = "InputAction", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> TrowAction;
+	
+public:	
+
+	UPhysicsHandle();
+	
+protected:
+
+	virtual void BeginPlay() override;
+	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+
+	UFUNCTION()
+	void AttachToPhysicsHandleController(AMyCharacter* TargetCharacter);
+	
+	void ToggleGrabObject();
+	void TrowObject();
+	void ReleaseComponent() override;		
+	void GrabComponents();
+	void DontInteract();
+
+private:
+
+	UPROPERTY(NotBlueprintType)
+	TObjectPtr<AMyCharacter> Character;
 	
 };
