25b67220de2a5a560c1fc59f238b4de86a3acbaf
Olexsandr <fddftrrfg@gmail.com>
2024-04-05 18:44:26 +0300

    Flesh light

---
+++ Source/JobsGame/Private/PlayerComponent/FleshLightComponent.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SpotLightComponent.h"
+#include "FleshLightComponent.generated.h"
+
+
+UCLASS()
+class UFleshLightComponent : public USpotLightComponent
+{
+	GENERATED_BODY()
+	
+};
