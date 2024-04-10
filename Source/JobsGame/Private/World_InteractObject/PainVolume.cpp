185429d378c7a51f8f9bee698f187632faa7233d
Olexsandr <fddftrrfg@gmail.com>
2024-04-10 23:27:31 +0300

    PainCausing work

--- Source/JobsGame/Private/World_InteractObject/PainVolume.cpp
+++ Source/JobsGame/Private/World_InteractObject/PainVolume.cpp
@@ -1,5 +1,89 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "World_InteractObject/PainVolume.h"
+#include "Engine/DamageEvents.h"
 
+APainVolume::APainVolume(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+
+	bPainCausing = true;
+	DamageType = UDamageType::StaticClass();
+	DamagePerSec = 1.0f;
+	bEntryPain = true;
+	PainInterval = 1.0f;
+	
+}
+
+
+void APainVolume::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	BACKUP_bPainCausing	= bPainCausing;
+
+}
+
+void APainVolume::ActorEnteredVolume(AActor* Other)
+{
+	Super::ActorEnteredVolume(Other);
+
+	if ( bPainCausing && bEntryPain && Other->CanBeDamaged() )
+	{
+		CausePainTo(Other);
+	}
+
+	// Start timer if none is active
+	if (!GetWorldTimerManager().IsTimerActive(TimerHandle_PainTimer))
+	{
+		GetWorldTimerManager().SetTimer(TimerHandle_PainTimer, this, &APainCausingVolume::PainTimer, PainInterval, true);
+	}
+
+}
+
+
+void APainVolume::CausePainTo(AActor* Other)
+{
+	Super::CausePainTo(Other);
+
+	if (DamagePerSec > 0.f)
+	{
+		TSubclassOf<UDamageType> DmgTypeClass = DamageType ? *DamageType : UDamageType::StaticClass();
+		Other->TakeDamage(DamagePerSec*PainInterval, FDamageEvent(DmgTypeClass), DamageInstigator, this);
+	}
+	
+}
+
+void APainVolume::PainTimer()
+{
+	Super::PainTimer();
+
+	if (bPainCausing)
+	{
+		TSet<AActor*> TouchingActors;
+		GetOverlappingActors(TouchingActors);
+
+		for (AActor* const A : TouchingActors)
+		{
+			if (IsValid(A) && A->CanBeDamaged() && A->GetPhysicsVolume() == this)
+			{
+				CausePainTo(A);
+			}
+		}
+
+		// Stop timer if nothing is overlapping us
+		if (TouchingActors.Num() == 0)
+		{
+			GetWorldTimerManager().ClearTimer(TimerHandle_PainTimer);
+		}
+	}
+
+}
+
+
+void APainVolume::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	GetWorldTimerManager().ClearTimer(TimerHandle_PainTimer);
+}
--- Source/JobsGame/Public/World_InteractObject/PainVolume.h
+++ Source/JobsGame/Public/World_InteractObject/PainVolume.h
@@ -5,11 +5,66 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PainCausingVolume.h"
 #include "PainVolume.generated.h"
+class UDamageType;
+class AController;
 
+USTRUCT()
+struct FLoadSound
+{
+	GENERATED_BODY()
+	
+};
 
 UCLASS()
 class JOBSGAME_API APainVolume : public APainCausingVolume
 {
 	GENERATED_BODY()
+
+	/** Whether volume currently causes damage. */
+	UPROPERTY(EditAnywhere, Category=PainCausingVolume)
+	uint16 PainCausing:1;
+
+	/** Damage done per second to actors in this volume when bPainCausing=true */
+	UPROPERTY(EditAnywhere, Category=PainCausingVolume)
+	float DamagePerSecond;
+
+	/** Type of damage done */
+	UPROPERTY(EditAnywhere, Category=PainCausingVolume)
+	TSubclassOf<UDamageType> DamageTypes;
+
+	/** If pain causing, time between damage applications. */
+	UPROPERTY(EditAnywhere, Category=PainCausingVolume)
+	float PainIntervals;
+
+	/** if bPainCausing, cause pain when something enters the volume in addition to damage each second */
+	UPROPERTY(EditAnywhere, Category=PainCausingVolume)
+	uint16 blsEntryPain:1;
+
+	/** Checkpointed bPainCausing value */
+	UPROPERTY()
+	uint16 BACKUP_bPainCausings:1;
+
+	/** Controller that gets credit for any damage caused by this volume */
+	UPROPERTY()
+	TObjectPtr<AController> DamageInstigators;
+
+public:
+
+	APainVolume(const FObjectInitializer& ObjectInitializer);
+
+protected:
+
+	void CausePainTo(class AActor* Other);
+
+	FTimerHandle TimerHandle_PainTimer;
 	
+public:
+
+	virtual void PainTimer() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+		
+	virtual void PostInitializeComponents() override;
+	
+	void ActorEnteredVolume(AActor* Other);
 };
