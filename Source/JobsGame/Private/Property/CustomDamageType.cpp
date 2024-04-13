8a7a0109a2d58b52b3416a6f9e35b01e38802684
Olexsandr <fddftrrfg@gmail.com>
2024-04-13 18:13:15 +0300

    Damage types

--- Source/JobsGame/Private/Property/CustomDamageType.cpp
+++ Source/JobsGame/Private/Property/CustomDamageType.cpp
@@ -1,5 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Property/CustomDamageType.h"
 
+DEFINE_LOG_CATEGORY(LogCustomDamage)
+
+UCustomDamageType::UCustomDamageType()
+{
+	// Initialize the dictionary with default values for each damage type
+	DamageData.Add(EDamageType::DMG_FIRE, FDamageTypeData{1.0f, 0.0f});
+	DamageData.Add(EDamageType::DMG_ELECTRIC, FDamageTypeData{1.0f, 0.0f});
+	DamageData.Add(EDamageType::DMG_FALL, FDamageTypeData{1.0f, 0.0f});
+	DamageData.Add(EDamageType::DMG_DROWN, FDamageTypeData{1.0f, 0.0f});
+	DamageData.Add(EDamageType::DMG_WEAPON, FDamageTypeData{1.0f, 0.0f});
+	DamageData.Add(EDamageType::DMG_PHYSICS, FDamageTypeData{1.0f, 0.0f});
+	DamageData.Add(EDamageType::DMG_EXPLOSION, FDamageTypeData{1.0f, 0.0f});
+	
+}
+
+// Get data for the current damage type
+FDamageTypeData UCustomDamageType::GetDamageTypeData() const
+{
+	const FDamageTypeData* Data = DamageData.Find(DamageType);
+	return Data ? *Data:GetDamageTypeData();
+}
--- Source/JobsGame/Private/Property/CustomDamageType.h
+++ Source/JobsGame/Private/Property/CustomDamageType.h
@@ -6,10 +6,59 @@
 #include "GameFramework/DamageType.h"
 #include "CustomDamageType.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogCustomDamage, Log, All);
+
+// Struct Damage Setting 
+USTRUCT()
+struct FDamageTypeData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Blueprintable)
+	float DamageMultiplayer;
+	UPROPERTY(Blueprintable)
+	float EffectDuration;
+
+	FDamageTypeData(){};
+	FDamageTypeData(float DamageMultiplayer, float EffectDuration)
+	: DamageMultiplayer(DamageMultiplayer), EffectDuration(EffectDuration){}
+
+};
+
+// damage type
+UENUM(Blueprintable)
+enum class EDamageType : uint8
+{
+	DMG_FIRE UMETA(DisplayName = "FireDamage"),	
+	DMG_ELECTRIC UMETA(DisplayName = "ElectricDamage"),	
+	DMG_PHYSICS UMETA(DisplayName = "PhysicsDamage"),	
+	DMG_DROWN UMETA(DisplayName = "WaterDamage"),	
+	DMG_FALL UMETA(DisplayName = "FallDamage"),	
+	DMG_EXPLOSION UMETA(DisplayName = "ExplosionDamage"),	
+	DMG_WEAPON UMETA(DisplayName = "ExplosionDamage"),
+
+};
+
 
 UCLASS()
 class UCustomDamageType : public UDamageType
 {
 	GENERATED_BODY()
+
+	// Type Damage
+	UPROPERTY(EditAnywhere, Blueprintable, Category = "Damage")
+	EDamageType DamageType;
+
+	//Dictionary for storing data for each damage type
+	UPROPERTY(EditAnywhere, Blueprintable, Category = "Damage")
+	TMap<EDamageType, FDamageTypeData> DamageData;
+
+	// Get data for the current damage type
+	FDamageTypeData GetDamageTypeData() const;
+
+	
+public:
+	
+	UCustomDamageType();
 	
 };
