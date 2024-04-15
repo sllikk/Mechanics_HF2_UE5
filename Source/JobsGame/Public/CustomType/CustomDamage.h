fd64281b6dbc2d41d4790e62a03ce0e93dc2e97e
Olexsandr <fddftrrfg@gmail.com>
2024-04-15 17:41:10 +0300

    CustomDamage and fix all engine bugs

---
+++ Source/JobsGame/Public/CustomType/CustomDamage.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CustomDamage.generated.h"
+
+  
+ 
+UCLASS(meta=(ScriptName = "CustomDamage"))
+class JOBSGAME_API UCustomDamage : public UObject
+{
+	GENERATED_BODY()
+	
+
+	UCustomDamage();
+
+};
