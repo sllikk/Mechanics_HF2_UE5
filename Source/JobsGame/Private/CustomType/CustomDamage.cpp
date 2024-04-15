6c3a5f6145cebc19e2f7c97144cc380fef1db981
Olexsandr <fddftrrfg@gmail.com>
2024-04-15 18:46:35 +0300

    WORK VERSION

--- Source/JobsGame/Private/CustomType/CustomDamage.cpp
+++ Source/JobsGame/Private/CustomType/CustomDamage.cpp
@@ -1,9 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "CustomType/CustomDamage.h"
+
+DEFINE_LOG_CATEGORY(LogCustomDamage);
 
 UCustomDamage::UCustomDamage()
 {
+	DataType.Add(EDamageType::DMG_FIRE, FDamageTypeData{1.0f, 0.0f});
+	DataType.Add(EDamageType::DMG_ELECTRIC, FDamageTypeData{1.0f, 0.0f});
+	DataType.Add(EDamageType::DMG_FALL, FDamageTypeData{1.0f, 0.0f});
+	DataType.Add(EDamageType::DMG_DROWN, FDamageTypeData{1.0f, 0.0f});
+	DataType.Add(EDamageType::DMG_EXPLOSION, FDamageTypeData{1.0f, 0.0f});
+	DataType.Add(EDamageType::DMG_PHYSICS, FDamageTypeData{1.0f, 0.0f});
+	DataType.Add(EDamageType::DMG_WEAPON, FDamageTypeData{1.0f, 0.0f});
+	
+}
+
+
+FDamageTypeData UCustomDamage::GetDamageTypeData() const
+{
+	const FDamageTypeData* Data = DataType.Find(DamageType);
+	return Data ? *Data : GetDamageTypeData();
 
 }
--- Source/JobsGame/Public/CustomType/CustomDamage.h
+++ Source/JobsGame/Public/CustomType/CustomDamage.h
@@ -6,14 +6,62 @@
 #include "UObject/NoExportTypes.h"
 #include "CustomDamage.generated.h"
 
-  
- 
-UCLASS(meta=(ScriptName = "CustomDamage"))
+DECLARE_LOG_CATEGORY_EXTERN(LogCustomDamage, Log, All);
+
+UENUM(BlueprintType, meta=(ScriptName = "ECustomDamage"))
+enum class EDamageType
+{
+	DMG_FIRE				UMETA(DisplayName = "FireDamage"),
+	DMG_ELECTRIC			UMETA(DisplayName = "ElectricDamage"),
+	DMG_FALL				UMETA(DisplayName = "FallDamage"),
+	DMG_DROWN				UMETA(DisplayName = "DrownDamage"),
+	DMG_EXPLOSION			UMETA(DisplayName = "ExplosionDamage"),
+	DMG_PHYSICS				UMETA(DisplayName = "PhysicsDamage"),
+	DMG_WEAPON				UMETA(DisplayName = "WeaponDamage"),
+	
+};
+
+USTRUCT(BlueprintType)
+struct FDamageTypeData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(Blueprintable)
+	float DamageMultiplayer; 
+	UPROPERTY(Blueprintable)
+	float DamageEffects;
+
+	FDamageTypeData()
+	   : DamageMultiplayer(0.0f),
+		 DamageEffects(0.0f)
+	{}
+
+	FDamageTypeData(float damageMultiplayer, float effectDamage)
+		: DamageMultiplayer(damageMultiplayer),DamageEffects(effectDamage)
+	{}
+};
+
+UCLASS(BlueprintType, meta=(ScriptName = "CustomDamage"))
 class JOBSGAME_API UCustomDamage : public UObject
 {
 	GENERATED_BODY()
 	
+public:
 
 	UCustomDamage();
 
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DamageType",  meta = (ScriptName = "CustomDamageType"))
+	EDamageType DamageType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DamageTypeData")
+	FDamageTypeData DamageTypeData;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DataType")
+	TMap<EDamageType, FDamageTypeData> DataType;
+
+	FDamageTypeData GetDamageTypeData() const;
+	
+	
 };
