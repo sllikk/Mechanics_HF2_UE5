970ffa86cfe1962c28ce4bb4d130d65f089e19cc
Olexsandr <fddftrrfg@gmail.com>
2024-04-20 20:28:51 +0300

    HotFix

--- Source/JobsGame/Private/WorldActors/Health_Charger.cpp
+++ Source/JobsGame/Private/WorldActors/Health_Charger.cpp
@@ -1,14 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "WorldActors/Health_Charger.h"
+
+DEFINE_LOG_CATEGORY(LogHealthCharger);
 
 // Sets default values
 AHealth_Charger::AHealth_Charger()
 {
- 
  	 PrimaryActorTick.bCanEverTick = true;
 
+	HealthChargerMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("UStaticMeshComponent"));
+	FSoftObjectPath FindMesh(TEXT("/Game/World_InteractObject/HealthStation/health_charger"));
+	UStaticMesh* StaticMesh = nullptr;
+
+	if (FindMesh.IsValid())
+	{
+	     StaticMesh = Cast<UStaticMesh>(FindMesh.TryLoad());
+    }
+    if (StaticMesh != nullptr)
+    {
+		HealthChargerMeshComponent->SetStaticMesh(StaticMesh);			   
+    }
+    else
+    {
+		UE_LOG(LogHealthCharger, Warning, TEXT("Error Load: %s"), *FindMesh.ToString());
+    }
+
+	HealthChargerMeshComponent->SetWorldScale3D(FVector(40.0f,40.0f,40.0f));
+	
+	
 }
 
 
--- Source/JobsGame/Public/WorldActors/Health_Charger.h
+++ Source/JobsGame/Public/WorldActors/Health_Charger.h
@@ -6,12 +6,25 @@
 #include "GameFramework/Actor.h"
 #include "Health_Charger.generated.h"
 class AMyCharacter;
+class UStaticMeshComponent;
+class UBoxComponent;
+class UPointLightComponent;
 
+DECLARE_LOG_CATEGORY_EXTERN(LogHealthCharger, Log, All);
 
 UCLASS(Config=Game)
 class JOBSGAME_API AHealth_Charger : public AActor
 {
 	GENERATED_BODY()
+	
+	UPROPERTY(VisibleAnywhere, Category="MeshComponent", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> HealthChargerMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category="MeshComponent", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> BoxComponent;
+
+	UPROPERTY(VisibleAnywhere, Category="MeshComponent", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UPointLightComponent> LightCharger;
 	
 public:	
 
