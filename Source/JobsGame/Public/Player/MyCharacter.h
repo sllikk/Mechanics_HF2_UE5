457b71aef0184b0fa36d9c1862232dd639e4a154
Olexsandr <fddftrrfg@gmail.com>
2024-04-26 13:12:15 +0300

    fix

--- Source/JobsGame/Public/Player/MyCharacter.h
+++ Source/JobsGame/Public/Player/MyCharacter.h
@@ -107,7 +107,7 @@ public:
 
 	FORCEINLINE	USkeletalMeshComponent*		GetMesh1P() const								 { return Mesh1P; }
 	FORCEINLINE	UCameraComponent*			GetFirstPersonCamera() const					 { return FirstPersonCamera; }
-
+	/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
 	FORCEINLINE float						GetMaxSpeedWalk() const							 { return m_MaxSpeedWalk; }
 	FORCEINLINE void						SetMaxSpeedWalk(float fl_MaxSpeedWalk)			 { m_MaxSpeedWalk = fl_MaxSpeedWalk; }
 	FORCEINLINE float						GetMaxSpeedRun() const							 { return m_MaxSpeedRun; }
@@ -130,28 +130,48 @@ public:
 	FORCEINLINE	void						SetLineTraceLength(float fl_LineTraceLength)	 { m_LineTraceLength = fl_LineTraceLength; }
 	FORCEINLINE	float						GetDistanceTrace() const						 { return m_DistanceTrace; }
 	FORCEINLINE	void						SetDistanceTrace(float fl_DistanceTrace)	     { m_DistanceTrace = fl_DistanceTrace; }
-	FORCEINLINE	float						GetMaxGrabMassObject() const					 { return m_MaxGrabMassObject; }
-	FORCEINLINE	void						SetMaxGrabMassObject(float fl_MaxGrabMassObject) { m_MaxGrabMassObject = fl_MaxGrabMassObject; }
-	FORCEINLINE	float						GetTrowImpulse() const							 { return m_TrowImpulse; }
-	FORCEINLINE	void						SetTrowImpulse(float fl_TrowImpulse)			 { m_TrowImpulse = fl_TrowImpulse; }
 	
+	/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+											// Physics Handle
+	/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+	FORCEINLINE	float						GetMaxGrabMassObject() const									{ return m_MaxGrabMassObject; }
+	FORCEINLINE	void						SetMaxGrabMassObject(float fl_MaxGrabMassObject)				{ m_MaxGrabMassObject = fl_MaxGrabMassObject; }
+	FORCEINLINE	float						GetTrowImpulse() const											{ return m_TrowImpulse; }
+	FORCEINLINE	void						SetTrowImpulse(float fl_TrowImpulse)							{ m_TrowImpulse = fl_TrowImpulse; }
+	FORCEINLINE float						GetLinearDamping() const										{ return m_LinearDamping; }
+	FORCEINLINE void						SetLinearDamping(float fl_LinearDamping)						{ m_LinearDamping = fl_LinearDamping; }
+	FORCEINLINE	float						GetLinearStiffness() const										{ return m_LinearStiffness; }
+	FORCEINLINE	void						SetLinearStiffness(float fl_LinearStiffness)					{ m_LinearStiffness = fl_LinearStiffness; }
+	FORCEINLINE	float						GetAngularDamping() const										{ return m_AngularDamping; }
+	FORCEINLINE	void						SetAngularDamping(float fl_AngularDamping)						{ m_AngularDamping = fl_AngularDamping; }
+	FORCEINLINE	float						GetAngularStiffness() const										{ return m_AngularStiffness; }
+	FORCEINLINE	void						SetAngularStiffness(float fl_AngularStiffness)					{ m_AngularStiffness = fl_AngularStiffness; }
+	FORCEINLINE	float						GetInterpolationSpeed() const									{ return m_InterpolationSpeed; }
+	FORCEINLINE	void						SetInterpolationSpeed(float fl_InterpolationSpeed)				{ m_InterpolationSpeed = fl_InterpolationSpeed; }
+	FORCEINLINE	bool						GetIsSoftAngularConstraint() const								{ return m_blsSoftAngularConstraint; }
+	FORCEINLINE	void						SetIsSoftAngularConstraint(bool blsIsSoftAngularConstraint)		{ m_blsSoftAngularConstraint = blsIsSoftAngularConstraint; }
+	FORCEINLINE	bool						GetIsSoftLinearConstraint() const { return m_blsSoftLinearConstraint; }
+	FORCEINLINE	void						SetIsSoftLinearConstraint(bool blsIsSoftLinearConstraint)		{ m_blsSoftLinearConstraint = blsIsSoftLinearConstraint; }
+	FORCEINLINE	bool						GetInterpolateTarget() const { return m_blsInterpolateTarget; }
+	FORCEINLINE	void						SetInterpolateTarget(bool blsInterpolateTarget)					{ m_blsInterpolateTarget = blsInterpolateTarget; }
+
 	# pragma endregion
 	
 #pragma region FUNC_INPUT
 
-	void Move(const FInputActionValue& Value);
-	void Look(const FInputActionValue& Value);
-	void Run();
-	void StopRun();
-	void StartCrouch();
-	void StopCrouch();
-	void Interact();
-	void Flashlight();
-	void ToggleGrab();
-	void GrabComponent();
-	void ReleaseComponent();
-	void DontInteract();
-	void TrowObject();
+	void	Move(const FInputActionValue& Value);
+	void	Look(const FInputActionValue& Value);
+	void	Run();
+	void	StopRun();
+	void	StartCrouch();
+	void	StopCrouch();
+	void	Interact();
+	void	Flashlight();
+	void	ToggleGrab();
+	void	GrabComponent();
+	void	ReleaseComponent();
+	void	DontInteract();
+	void	TrowObject();
 	
 	#pragma endregion
 
@@ -159,41 +179,31 @@ private:
 
 	#pragma region Default_Character_Settings
 
-	UPROPERTY(Config)
-	float m_MaxSpeedWalk = 500.0f;
-	UPROPERTY(Config)
-	float m_MaxSpeedRun = 700.0f;
-	UPROPERTY(Config)
-	float m_MaxSpeedCrouch = 400.0f;
-	UPROPERTY(Config)
-	float m_MaxAcceleration = 2048.0f;        
-	UPROPERTY(Config)
-	float m_GravityScale = 1.0f;
-	UPROPERTY(Config)
-	float m_AirControl = 0.8f;
-	UPROPERTY(Config)
-	float m_MaxSpeedFly = 200.0f;
-	UPROPERTY(Config)
-	float m_MassCharacter = 20.0f;
-	UPROPERTY(Config)
-	float m_JumpHeight = 300.0f;
-	UPROPERTY(Config)
-	float m_LineTraceLength = 180.0f;
-	UPROPERTY(Config)
-	float m_DistanceTrace = 190;
-	UPROPERTY(Config)
-	float m_MaxGrabMassObject = 80;	
-	UPROPERTY(Config)
-	float m_TrowImpulse = 250;
+	float		m_MaxSpeedWalk = 500.0f;
+	float		m_MaxSpeedRun = 700.0f;
+	float		m_MaxSpeedCrouch = 400.0f;
+	float		m_MaxAcceleration = 2048.0f;        
+	float		m_GravityScale = 1.0f;
+	float		m_AirControl = 0.8f;
+	float		m_MaxSpeedFly = 200.0f;
+	float		m_MassCharacter = 20.0f;
+	float		m_JumpHeight = 300.0f;
+	float		m_LineTraceLength = 180.0f;
+	float		m_DistanceTrace = 190;
+	float		m_MaxGrabMassObject = 80;	
+	float		m_TrowImpulse = 250;
+	float       m_LinearDamping;
+	float		m_LinearStiffness = 750.0f;
+	float		m_AngularDamping = 500.0f;
+	float		m_AngularStiffness = 1500.0f;
+	float		m_InterpolationSpeed = 50.0f;
+	bool		m_blsSoftAngularConstraint = true;
+	bool		m_blsSoftLinearConstraint = true;
+	bool		m_blsInterpolateTarget = true;
+
 
 	#pragma endregion
 	
-	// Audio my Character
-	UPROPERTY(VisibleAnywhere, Category = "Components")
-	TObjectPtr<USoundBase> SoundBase;
-	
-	
-	bool blsDoorInteract;
 
 
 };
