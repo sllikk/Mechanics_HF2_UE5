3455679d76ca1e10cc0169d6c9b700cfe01f9281
Olexsandr <fddftrrfg@gmail.com>
2024-05-02 11:22:53 +0300

    AI

---
+++ Source/JobsGame/Private/World_InteractObject/hopper_mine.cpp
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "World_InteractObject/hopper_mine.h"
+
+#include "Components/PointLightComponent.h"
+#include "Particles/ParticleSystem.h"
+#define HOPPER_MINE_MAX_FLIPS 5
+
+
+Ahopper_mine::Ahopper_mine()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Default SubObject
+	Mine_SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	Detected_Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLightComponent"));
+	
+	// Load Mesh
+	static const FSoftObjectPath FinderMesh(TEXT("/Game/World_InteractObject/HopperMine/Combine_mine"));
+	USkeletalMesh* SkeletalMesh = nullptr;
+	if (FinderMesh.IsValid())
+	{
+		SkeletalMesh = Cast<USkeletalMesh>(FinderMesh.TryLoad());
+	}
+	if (SkeletalMesh != nullptr)
+	{
+		Mine_SkeletalMesh->SetSkeletalMesh(SkeletalMesh);
+	}
+
+	// Load Partecles 
+	static const FSoftObjectPath FinderParticle(TEXT("/Game/VFX/Particles/Explosion/Hopper_Explosion"));
+	UParticleSystem* FoundParticle = nullptr;
+
+	if(FinderParticle.IsValid())
+	{
+		FoundParticle = Cast<UParticleSystem>(FinderParticle.TryLoad());
+	}
+	if (FoundParticle != nullptr)
+	{
+		ExplodeParticle = FoundParticle;
+	}
+
+	SetRootComponent(Mine_SkeletalMesh);
+	Mine_SkeletalMesh->SetWorldScale3D(FVector(0.5f, 0.5f, 0.5f));
+	
+	
+}
+
+void Ahopper_mine::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void Ahopper_mine::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void Ahopper_mine::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+
---
+++ Source/JobsGame/Public/World_InteractObject/hopper_mine.h
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "hopper_mine.generated.h"
+class USkeletalMeshComponent;
+class UPointLightComponent;
+class UParticleSystem;
+
+UCLASS()
+class JOBSGAME_API Ahopper_mine : public APawn
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, Category="SkeletalMesh", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<USkeletalMeshComponent> Mine_SkeletalMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintType, Category="PointLight", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UPointLightComponent> Detected_Light;
+
+	UPROPERTY(EditAnywhere, Category="ParticleComponent", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UParticleSystem> ExplodeParticle;
+	
+public:
+
+	Ahopper_mine();
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	
+public:
+	
+	/*
+	void	Spawn();
+	void	OnRestore();
+	void	SetMineState( int iState );
+	int		GetMineState() { return m_iMineState; }
+	bool	IsValidLocation();
+	void	Flip( const FVector &vecForce, const AngularImpulse &torque );
+	void	SearchThink();
+	void	BounceThink();
+	void	 SettleThink();
+	void	CaptiveThink();
+	void	ExplodeThink();
+	void	ExplodeTouch( CBaseEntity *pOther );
+	void	CavernBounceThink(); ///< an alternative style of bouncing used for the citizen modded bouncers
+	bool	IsAwake() { return m_bAwake; }
+	void	Wake( bool bWake );
+
+*/
+
+
+protected:
+
+	int8			m_iHookN;
+	int8			m_iHookE;
+	int8			m_iHookS;
+	int8			m_iAllHooks;
+	int8			m_iMineState;
+	int8			m_iFlipAttempts;
+	int8			m_iModification;
+	
+	float		m_flIgnoreWorldTime;
+	float		m_flExplosionDelay;
+	float		m_flHookPositions;
+	float		m_flLastPhysicsInfluenceTime;
+	float		m_flTimeGrabbed;
+
+	bool		m_bDisarmed;
+	bool		m_bLockSilently;
+	bool		m_bFoeNearest;
+	bool		m_bPlacedByPlayer;
+	bool		m_bHeldByPhysgun;
+
+};
