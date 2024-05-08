132e6ca400caf23afe3f14b824784366592bcbb4
Olexsandr <fddftrrfg@gmail.com>
2024-05-08 23:31:18 +0300

    AI_CONTROLLER

--- Source/JobsGame/Private/AI/combine_ai/AI_Combine.cpp
+++ Source/JobsGame/Private/AI/combine_ai/AI_Combine.cpp
@@ -3,3 +3,51 @@
 
 #include "AI/combine_ai/AI_Combine.h"
 
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "NPC/npc_combine.h"
+#include "Perception/AIPerceptionComponent.h"
+
+AAI_Combine::AAI_Combine(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent")));	
+	// Остальная инициализац
+
+	
+}
+
+
+void AAI_Combine::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+
+void AAI_Combine::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AAI_Combine::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	const Anpc_combine* npc_combine = Cast<Anpc_combine>(InPawn);
+
+	if (npc_combine != nullptr && npc_combine->TreeAsset != nullptr)
+	{
+		BBC->InitializeBlackboard(*npc_combine->TreeAsset->BlackboardAsset);		
+		BTC->StartTree(*npc_combine->TreeAsset);
+	}
+	
+}
+
+void AAI_Combine::OnTargetUpdate(AActor* Actor, FAIStimulus Stimulus)
+{
+
+}
+
+
+
--- Source/JobsGame/Private/AI/combine_ai/AI_Combine.h
+++ Source/JobsGame/Private/AI/combine_ai/AI_Combine.h
@@ -4,12 +4,44 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "AI_Combine.generated.h"
-
+class UBehaviorTreeComponent;
+class UBlackboardComponent;
+class UAISenseConfig_Sight;
+class UAISenseConfig_Hearing;
 
 UCLASS()
 class AAI_Combine : public AAIController
 {
 	GENERATED_BODY()
+
+	// Sense Perception config
+	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category="Sense_ConfigHearing", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UAISenseConfig_Hearing> Config_Hearing;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite ,Category="Sense_ConfigSight", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UAISenseConfig_Sight> Config_Sight;
+
+public:
+
+	AAI_Combine(const FObjectInitializer& ObjectInitializer);
+	
+protected:
+	
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	void OnTargetUpdate(AActor* Actor, FAIStimulus Stimulus);
+
+public:
+
+	// Behavior tree setup
+	UPROPERTY(EditAnywhere, Category="AI")
+	TObjectPtr<UBehaviorTreeComponent> BTC;
+
+	UPROPERTY(EditAnywhere, Category="AI")
+	TObjectPtr<UBlackboardComponent> BBC;
 	
 };
