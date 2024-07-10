096fbd1286903f92dac5ae5b06fa485ee763a1c9
Olexsandr <fddftrrfg@gmail.com>
2024-07-11 00:18:44 +0300

    base models and npc_turretfloor

---
+++ Source/JobsGame/Private/NPC/npc_turretfloor.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC/npc_turretfloor.h"
+
+
+// Sets default values
+Anpc_turretfloor::Anpc_turretfloor()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void Anpc_turretfloor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Anpc_turretfloor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
---
+++ Source/JobsGame/Public/NPC/npc_turretfloor.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "npc_turretfloor.generated.h"
+
+UCLASS()
+class JOBSGAME_API Anpc_turretfloor : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	Anpc_turretfloor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+};
