d1f10c192056de6e4b738dc92c3c07c4c401b017
Olexsandr <fddftrrfg@gmail.com>
2024-07-15 22:44:18 +0300

    turret dev

---
+++ Source/JobsGame/Private/NPC/npc_turret_floor.cpp
@@ -0,0 +1,131 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC/npc_turret_floor.h"
+#include "Components/PointLightComponent.h"
+#include "Components/PoseableMeshComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+// Sets default values
+Anpc_turret_floor::Anpc_turret_floor()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	turret_mesh = CreateDefaultSubobject<UPoseableMeshComponent>(TEXT("Turret_Mesh"));
+	RootComponent = turret_mesh;
+
+	turret_mesh->SetWorldScale3D(FVector(0.9f, 0.9f, 0.9f));
+	
+	const FSoftObjectPath ObjectPath(TEXT("/Game/World_InteractObject/Turret/turret_floor_model"));
+	static USkeletalMesh* LoadSkeletal = nullptr;
+	if (ObjectPath.IsValid())
+	{
+		LoadSkeletal = Cast<USkeletalMesh>(ObjectPath.TryLoad());
+	} 
+	if (LoadSkeletal != nullptr)
+	{
+		turret_mesh->SetSkinnedAsset(LoadSkeletal);
+	}
+	
+	detected_light = CreateDefaultSubobject<UPointLightComponent>(TEXT("Detected_Light"));
+	detected_light->SetupAttachment(turret_mesh);
+	detected_light->SetVisibility(true);
+	detected_light->AttenuationRadius = 60.0f;
+	detected_light->Intensity = 150;
+	
+	PerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComponent"));
+	Config_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Config_Sight"));
+	Config_Sight->SightRadius = 1500.0f;
+	Config_Sight->PeripheralVisionAngleDegrees = 15.0f;
+	Config_Sight->SetMaxAge(5.0f);
+	Config_Sight->LoseSightRadius = Config_Sight->SightRadius + 150;
+	Config_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	Config_Sight->DetectionByAffiliation.bDetectFriendlies = true;
+	Config_Sight->DetectionByAffiliation.bDetectNeutrals = true;
+
+	PerceptionComponent->SetDominantSense(*Config_Sight->GetSenseImplementation());
+	PerceptionComponent->ConfigureSense(*Config_Sight);
+	
+}
+
+// Called when the game starts or when spawned
+void Anpc_turret_floor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	detected_light->AttachToComponent(turret_mesh, FAttachmentTransformRules::SnapToTargetIncludingScale, "Light_Detected");
+}
+
+// Called every frame
+void Anpc_turret_floor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void Anpc_turret_floor::GetActorEyesViewPoint(FVector& OutLocation, FRotator& OutRotation) const
+{
+	//Super::GetActorEyesViewPoint(OutLocation, OutRotation);
+
+	OutLocation = turret_mesh->GetSocketLocation("ViewSocket");
+	OutRotation = GetActorRotation();
+	OutRotation = turret_mesh->GetSocketTransform("ViewSocket", RTS_ParentBoneSpace).Rotator();
+	
+	
+}
+
+void Anpc_turret_floor::Retire()
+{
+}
+
+void Anpc_turret_floor::Deploy()
+{
+}
+
+void Anpc_turret_floor::ActiveThink()
+{
+}
+
+void Anpc_turret_floor::SearchThink()
+{
+}
+
+void Anpc_turret_floor::AutoSearchThink()
+{
+}
+
+void Anpc_turret_floor::TippedThink()
+{
+}
+
+void Anpc_turret_floor::InactiveThink()
+{
+}
+
+void Anpc_turret_floor::SuppressThink()
+{
+}
+
+void Anpc_turret_floor::DisabledThink()
+{
+}
+
+void Anpc_turret_floor::SelfDestructThink()
+{
+}
+
+void Anpc_turret_floor::BreakThink()
+{
+}
+
+void Anpc_turret_floor::LightUpdate()
+{
+}
+
+void Anpc_turret_floor::SetTurretState(ETurretState State)
+{
+}
+
+
---
+++ Source/JobsGame/Public/NPC/npc_turret_floor.h
@@ -0,0 +1,99 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "npc_turret_floor.generated.h"
+
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+class UPointLightComponent;
+class UPoseableMeshComponent;
+class USoundBase;
+
+enum ETurretState
+{
+	TURRET_SEARCHING,
+	TURRET_AUTO_SEARCHING,
+	TURRET_ACTIVE,
+	TURRET_SUPPRESSING,
+	TURRET_DEPLOYING,
+	TURRET_RETIRING,
+	TURRET_TIPPED,
+	TURRET_SELF_DESTRUCTING,
+	TURRET_STATE_TOTAL
+};
+
+enum EDetectedState
+{
+	TURRET_EYE_SEE_TARGET,			//Sees the target, bright and big
+	TURRET_EYE_SEEKING_TARGET,		//Looking for a target, blinking (bright)
+	TURRET_EYE_DORMANT,				//Not active
+	TURRET_EYE_DEAD,				//Completely invisible
+	TURRET_EYE_DISABLED,			//Turned off, must be reactivated before it'll deploy again (completely invisible)
+	TURRET_EYE_ALARM,				// On side, but warning player to pick it back up
+};
+
+
+UCLASS()
+class JOBSGAME_API Anpc_turret_floor : public APawn
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Mesh", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UPoseableMeshComponent> turret_mesh;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Mesh", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UPointLightComponent> detected_light;
+	
+	UPROPERTY(VisibleAnywhere, Category="Ai", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UAIPerceptionComponent> PerceptionComponent;
+
+	UPROPERTY(EditAnywhere, Category="Mesh", meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UAISenseConfig_Sight> Config_Sight;
+
+	
+public:
+	// Sets default values for this pawn's properties
+	Anpc_turret_floor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void GetActorEyesViewPoint(FVector& OutLocation, FRotator& OutRotation) const override;
+	
+public:
+	
+	// Think functions
+	virtual void	Retire( void );
+	virtual void	Deploy( void );
+	virtual void	ActiveThink( void );
+	virtual void	SearchThink( void );
+	virtual void	AutoSearchThink( void );
+	virtual void	TippedThink( void );
+	virtual void	InactiveThink( void );
+	virtual void	SuppressThink( void );
+	virtual void	DisabledThink( void );
+	virtual void	SelfDestructThink( void );
+	virtual void	BreakThink( void );
+		
+			void LightUpdate();
+			void SetTurretState(ETurretState State);
+
+					
+private:
+
+	bool blstarget_detected;
+	
+	FVector vecTargetDirection;
+	FRotator RotCurrentBoneRotator;
+
+	float flattack_tracelenght;
+
+
+	
+};
