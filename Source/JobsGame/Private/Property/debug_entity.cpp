dbcaa4decaf0f263ff20eb0ace0c781738fa80ff
Olexsandr <fddftrrfg@gmail.com>
2024-07-23 00:44:39 +0300

    work debug entity

---
+++ Source/JobsGame/Private/Property/debug_entity.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Property/debug_entity.h"
+
+
+// Sets default values for this component's properties
+Udebug_entity::Udebug_entity(): DebugInfo()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void Udebug_entity::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void Udebug_entity::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	UpdateDebug();
+
+	
+}
+
+
+void Udebug_entity::UpdateDebug() const
+{	const FString& DEBUG = FString::Printf(TEXT("Name: %s\n"
+										   "Model: %s\n"
+										    "State: %s\n"
+										   "Position: %s\n"
+										   "Rotation: %s\n"
+										   "Mass: %.2f\n"
+										   "Stress: %.2f\n"
+										   "Health: %d\n"),
+											
+										*DebugInfo.Entity->GetName(),
+									   *DebugInfo.debug_mesh->GetName(),
+									   *DebugInfo.EnumAsString,		
+									   *FString(DebugInfo.debug_mesh->GetComponentLocation().ToString()),
+									   *FString(DebugInfo.debug_mesh->GetComponentRotation().ToString()),
+									   DebugInfo.debug_mesh->GetMass(),
+									   DebugInfo.flstressEntity, 	
+									   DebugInfo.ihealth);
+	//const FColor& ColorDebug = FColor::Silver;														
+	DrawDebugString(GetWorld(), DebugInfo.debug_mesh->GetComponentLocation(), DEBUG, nullptr, FColor::Silver, 0, true);		
+
+
+}
+
+FString Udebug_entity::GetEnumValueAsString(const FString& EnumName, int32 EnumValue)
+{
+	const UEnum* pEnum = FindObject<UEnum>(ANY_PACKAGE, *EnumName, true);
+	if(!pEnum) return FString("Invalid");
+
+	return pEnum->GetNameStringByIndex(EnumValue);
+}
+
+
---
+++ Source/JobsGame/Public/Property/debug_entity.h
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "debug_entity.generated.h"
+
+USTRUCT(Blueprintable, BlueprintType)
+struct FDebugInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	TObjectPtr<AActor> Entity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	TObjectPtr<UStaticMeshComponent> debug_mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	float flmassEntity;	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	float flstressEntity;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	int32 ihealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debug")
+	FString EnumAsString;
+};
+
+
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class JOBSGAME_API Udebug_entity : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	Udebug_entity();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+							   FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Debug")
+	void UpdateDebug() const;
+	 
+	UFUNCTION(BlueprintCallable)
+	void SetDebugEntity(AActor* DebugActor);
+
+	UFUNCTION(BlueprintCallable)
+	void SetDebugMesh(UStaticMeshComponent* DebugMesh);
+	
+	UFUNCTION(Blueprintable, BlueprintSetter)
+	FORCEINLINE void SetDebugHealth(int32 idebug) { DebugInfo.ihealth = idebug; }
+	
+	UFUNCTION(Blueprintable, BlueprintSetter)
+	FORCEINLINE void SetDebugStress(float fldebug) { DebugInfo.flstressEntity = fldebug; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE void SetDebugEnumAsString(const FString& strEnum) { DebugInfo.EnumAsString = strEnum; }
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Debug")
+	FDebugInfo DebugInfo;
+
+	UFUNCTION(BlueprintCallable)
+	FString GetEnumValueAsString(const FString& EnumName, int32 EnumValue); 
+	
+private:
+
+	
+};
+
+
+
+FORCEINLINE void Udebug_entity::SetDebugEntity(AActor* DebugActor)
+{
+	DebugInfo.Entity = DebugActor;
+}
+
+FORCEINLINE void Udebug_entity::SetDebugMesh(UStaticMeshComponent* DebugMesh)
+{
+	DebugInfo.debug_mesh = DebugMesh;
+}
